d715fbe8608cc54dad0027b1d90f77caad667e74
Nenad Seke <Nenad Seke@localhost>
2010-09-09 03:42:07 +0200

    changed remove product page

--- src/main/java/org/seke/fs/pages/RemoveProduct.java
+++ src/main/java/org/seke/fs/pages/RemoveProduct.java
@@ -1,5 +1,12 @@
 package org.seke.fs.pages;
 
+import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.seke.fs.Identifiable;
+import org.seke.fs.Product;
+import org.seke.fs.services.ProductsService;
+
 /**
  * Created by IntelliJ IDEA.
  * User: nenad
@@ -8,4 +15,33 @@ package org.seke.fs.pages;
  * To change this template use File | Settings | File Templates.
  */
 public class RemoveProduct {
+
+    @Property
+    private Product product;
+
+    @Inject
+    private ProductsService productsService;
+
+    public void onActivate(long id) {
+        product = productsService.retrieve(id);
+    }
+
+    long onPassivate() {
+        return Identifiable.class.cast(product).getId();
+    }
+
+    @OnEvent(value = "submit", component = "fRemoveProduct")
+    void saveProduct() {
+    }
+
+    @OnEvent(component = "yesButton")
+    void onYesButton() {
+        System.out.println("Product is disabled");
+    }
+
+    @OnEvent(component = "noButton")
+    void onNoButton() {
+        System.out.println("disabling product canceled!");
+    }
+
 }
